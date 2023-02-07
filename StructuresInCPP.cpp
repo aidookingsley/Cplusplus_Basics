5cbb7fecf269c7d6d8484b7d39114c2e1f2fa301
aidookingsley <kaidoog36@gmail.com>
2023-02-07 10:35:42 +0000

    basic structures

---
+++ StructuresInCPP.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+enum cow_purpose
+{
+    diary,
+    meat,
+    hide,
+    pet
+};
+
+struct cow
+{
+    string name;
+    int age;
+    unsigned char purpose;
+};
+
+int main()
+{
+    cow my_cow;
+    my_cow.age = 5;
+    my_cow.name = "Betsy";
+    my_cow.purpose = diary;
+    cout << my_cow.name << " is a type-" << (int)my_cow.purpose << " cow." << endl;
+    cout << my_cow.name << " is " << my_cow.age << " years old." << endl;
+    return 0;
+}
