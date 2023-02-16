bb8c9ce865aa6005fa41616d35be1560f1689ef8
aidookingsley <kaidoog36@gmail.com>
2023-02-16 07:18:44 +0000

    file I/O

---
+++ fileStream2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+ofstream outFile;
+float a = 4.333f, b = 5.302f;
+
+int main()
+{
+    outFile.open("calculations.txt");
+    if (outFile.fail())
+        cout << endl
+             << " Couldn't open the file!" << endl;
+    else
+    {
+        outFile << "a = " << a << endl;
+        outFile << "b = " << b << endl;
+        outFile << "a + b = " << a + b << endl;
+        outFile << "a * b = " << a * b << endl;
+        outFile << "a / b = " << a / b << endl;
+        outFile.close();
+        cout << "File written successfully!" << endl;
+    }
+    return 0;
+}
